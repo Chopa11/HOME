93540886faac24fa7206e72eb03e10b8d5abc258
Chopa11 <140246931+Chopa11@users.noreply.github.com>
2023-08-05 17:03:57 +0600

    Add files via upload

---
+++ Lesson2.cs
@@ -0,0 +1,44 @@
+﻿// Tasl5
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int num1 = num / 10;
+// int num2 = num1 % 10;
+// Console.WriteLine(num2);
+
+
+// Tasl6
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// while(num > 1000)
+// {
+//     num = num/10;
+// }
+
+// if((num>99) && (num <1000))
+// {
+//     num  = num % 10;
+//     Console.WriteLine(num);
+// }
+// else
+// {
+//     Console.WriteLine("3 числа нет");
+// }
+
+   
+// Tasl7
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+//  if (num<6)
+//  {
+//     Console.WriteLine("Будние дни");
+//  }
+//  else if ((num > 5) && ( num < 8))
+//  {
+//     Console.WriteLine("Выходные дни");
+//  } 
+//  else  {
+//     Console.WriteLine("Такого дня нет");
+//  }
+
