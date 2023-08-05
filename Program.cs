128e06fa5f2a40b4267eac01c0e86094a3c217da
Chelsea <in_pieces_jc@mail.ru>
2023-08-05 15:59:00 +0600

    First

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿// Task1
+// Console.WriteLine("Введите число");
+// int A = Convert.ToInt32(Console.ReadLine());
+// int B = Convert.ToInt32(Console.ReadLine());
+// if (A > B)
+// {
+//     Console.Write("Max = ");
+//     Console.WriteLine(A);
+//     Console.Write("Min = ");
+//     Console.WriteLine(B);
+// }
+// else 
+// {
+//     Console.Write("Max = ");
+//     Console.WriteLine(B);
+//     Console.Write("Min = ");
+//     Console.WriteLine(A);
+// }
+
+// Tasl2
+// Console.WriteLine("Введите число");
+// int A = Convert.ToInt32(Console.ReadLine());
+// int B = Convert.ToInt32(Console.ReadLine());
+// int C = Convert.ToInt32(Console.ReadLine());
+// int  max = A;
+// if (A > max) max = A;
+// if (B > max) max = B;
+// if (C > max) max = C;
+// Console.WriteLine("Max = ");
+// Console.WriteLine(max);
+
+
+//Tasl3
+// Console.WriteLine("Введите число");
+// int A = Convert.ToInt32(Console.ReadLine());
+
+// if (A%2==0)
+// {
+//     Console.WriteLine("Da");
+// }
+// else
+// {
+//     Console.WriteLine("No");
+// }
+
+// Tasl4
+// Console.WriteLine("Введите число");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// int i = 0;
+
+// while (i < N)
+// {
+//     if(i%2 == 0)
+//     {
+//         Console.WriteLine(i);
+//     }
+//     i ++;
+// }
+
+// Tasl5
+Console.WriteLine("Введите число");
