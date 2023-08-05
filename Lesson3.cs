4c901e2127ade515dcdf82443082eaed71bd8fe9
Chopa11 <140246931+Chopa11@users.noreply.github.com>
2023-08-05 18:00:54 +0600

    Add files via upload

---
+++ Lesson3.cs
@@ -0,0 +1,48 @@
+﻿// Task19
+// Console.WriteLine("Введите число");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// int num1 = num / 10000;
+// int num2 = (num / 1000) % 10;
+// int num3 = (num / 10) % 10;
+// int num4 = num % 10;
+
+// if((num1 == num4) && (num2 == num3))
+// {
+//   Console.WriteLine("Да");
+// }
+// else
+// {
+//   Console.WriteLine("Нет");
+// } 
+
+
+// Task21
+//  Console.WriteLine("Введите число");
+//  int X1 = Convert.ToInt32(Console.ReadLine());
+//  Console.WriteLine("Введите число");
+//  int Y1 = Convert.ToInt32(Console.ReadLine());
+//  Console.WriteLine("Введите число");
+//  int Z1 = Convert.ToInt32(Console.ReadLine());
+
+//  Console.WriteLine("Введите число");
+// int X2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int Y2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число");
+// int Z2 = Convert.ToInt32(Console.ReadLine());
+
+// double num = Math.Sqrt(Math.Pow((X2-X1), 2) + Math.Pow((Y2-Y1), 2) + Math.Pow((Z2-Z1), 2));
+// Console.WriteLine(num);
+   
+
+// Task23
+// Console.WriteLine("Введите число");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int i = 1;
+// while(i <= N)
+// {
+//     double num = Math.Pow(i, 3);
+//     i++; 
+//     Console.WriteLine(num);
+// }
